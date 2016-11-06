41c9897ae5acfdcaa73b3629372c938d2c68da4f
Ryan Stephens <ryan_stephens99@hotmail.com>
2016-11-05 23:44:11 -0400

    Card Objects can no longer attack themselves

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -26,6 +26,7 @@ public class Attack : MonoBehaviour {
         RaycastHit2D rayHit = Physics2D.Raycast(ray.origin, ray.direction);
 
         if (rayHit && rayHit.collider) {
+            if (gameObject.Equals(rayHit.collider.gameObject)) return;
             Health enemyHp = rayHit.collider.GetComponent<Health>();
             Attack enemy = rayHit.collider.GetComponent<Attack>();
             if (enemyHp) {
